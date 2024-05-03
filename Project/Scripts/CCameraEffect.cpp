ab0ee128f08ca52df9c84fd642a1ed8232e4e86b
jeontigger <boy9609@naver.com>
2024-05-04 01:26:09 +0900

    #10 Feat / 움직임 부드럽게 변경 (빈도수 추가)

--- Project/Scripts/CCameraEffect.cpp
+++ Project/Scripts/CCameraEffect.cpp
@@ -6,11 +6,15 @@
 CCameraEffect::CCameraEffect()
 	: CScript((UINT)SCRIPT_TYPE::CAMERAEFFECT)
 	, m_fShakeDuration(1.f)
-	, m_fShakeScale(1.f)
+	, m_vShakeIntensity(10.f, 10.f)
+	, m_fShakeFrequency(6.f)
 {
 	AppendScriptParam("ShakeDuration", SCRIPT_PARAM::FLOAT, &m_fShakeDuration, 0);
 	AppendScriptParam("ShakeTimer", SCRIPT_PARAM::FLOAT, &m_fShakeTimer, 0, 0, true);
-	AppendScriptParam("ShakeScale", SCRIPT_PARAM::FLOAT, &m_fShakeScale, 0);
+	AppendScriptParam("ShakeScale", SCRIPT_PARAM::VEC2, &m_vShakeIntensity, 0);
+
+	AppendScriptParam("ShakeFrequnecy", SCRIPT_PARAM::FLOAT, &m_fShakeFrequency, 0);
+	AppendScriptParam("ShakeFreqTimer", SCRIPT_PARAM::FLOAT, &m_fShakeFrequencyTimer, 0, 0, true);
 }
 
 CCameraEffect::~CCameraEffect()
@@ -23,24 +27,34 @@ void CCameraEffect::Shaking()
 	{
 		m_bShake = false;
 		m_fShakeTimer = 0.f;
+		m_fShakeFrequencyTimer = 0.f;
+
 		return;
 	}
 
-	Vec3 vPos = Transform()->GetRelativePos();
-	vPos.x = CRandomMgr::GetInst()->GetRandomFloat() * m_fShakeScale;
-	vPos.y = CRandomMgr::GetInst()->GetRandomFloat() * m_fShakeScale;
+	if (m_fShakeFrequencyTimer <= 0) {
+		m_fShakeFrequencyTimer += 1.f / m_fShakeFrequency;
+		m_vStartPos = Transform()->GetRelativePos();
 
-	Transform()->SetRelativePos(vPos);
+		m_vTargetPos = m_vStartPos;
+		m_vTargetPos.x += CRandomMgr::GetInst()->GetRandomFloat() * m_vShakeIntensity.x;
+		m_vTargetPos.y += CRandomMgr::GetInst()->GetRandomFloat() * m_vShakeIntensity.y;
+	}
+	Vec3 vNewPos = RoRMath::Lerp(m_vTargetPos, m_vStartPos, m_fShakeFrequencyTimer * m_fShakeFrequency);
+
+	Transform()->SetRelativePos(vNewPos);
 
 	m_fShakeTimer -= DT;
+	m_fShakeFrequencyTimer -= DT;
 }
 
-void CCameraEffect::Shake(float _duration, float _scale)
+void CCameraEffect::Shake(float _duration, Vec2 _scale)
 {
 	m_bShake = true;
 
 	m_fShakeTimer = _duration;
-	m_fShakeScale = _scale;
+	m_vShakeIntensity = _scale;
+	m_fShakeFrequencyTimer = 0;
 }
 
 
@@ -49,7 +63,7 @@ void CCameraEffect::tick()
 	// 테스트용 코드
 	if (KEY_TAP(T)) 
 	{
-		Shake(m_fShakeDuration, m_fShakeScale);
+		Shake(m_fShakeDuration, m_vShakeIntensity);
 	}
 
 	// 카메라 쉐이킹
--- Project/Scripts/CCameraEffect.h
+++ Project/Scripts/CCameraEffect.h
@@ -7,13 +7,18 @@ private:
     bool m_bShake;
     float m_fShakeTimer;
     float m_fShakeDuration;
-    float m_fShakeScale;
+    Vec2 m_vShakeIntensity;
+
+    float m_fShakeFrequencyTimer;
+    float m_fShakeFrequency;
+    Vec3 m_vStartPos;
+    Vec3 m_vTargetPos;
 
 private:
     void Shaking();
 
 public:
-    void Shake(float _duration, float _scale);
+    void Shake(float _duration, Vec2 _scale);
 
 public:
     virtual void tick() override;
