e74b0a1f87f25ba39ddce936a22fcd25b94b4b84
SEO DONG YUN <skdy1201@gmail.com>
2024-05-13 07:32:31 +0900

    #106 logmgr 함수 추가

--- Project/Engine/CLogMgr.cpp
+++ Project/Engine/CLogMgr.cpp
@@ -1,2 +1,30 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "CLogMgr.h"
+
+void CLogMgr::init()
+{
+}
+
+void CLogMgr::tick()
+{
+}
+
+void CLogMgr::enter()
+{
+}
+
+void CLogMgr::exit()
+{
+}
+
+void CLogMgr::AddLog()
+{
+}
+
+void CLogMgr::CopyLog()
+{
+}
+
+void CLogMgr::ClearLog()
+{
+}
--- Project/Engine/CLogMgr.h
+++ Project/Engine/CLogMgr.h
@@ -5,5 +5,20 @@ class CLogMgr :
     public CManager<CLogMgr>
 {
     SINGLE(CLogMgr)
+
+private:
+
+    vector<tLog> m_vecLog;
+
+public:
+    virtual void init() override;
+    virtual void tick() override;
+    virtual void enter() override;
+    virtual void exit() override;
+
+public:
+    void AddLog();
+    void CopyLog();
+    void ClearLog();
 };
 
