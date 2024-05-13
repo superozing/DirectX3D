60b132a23abdd5078016bb9ea60100b7bd9a8d55
JinWoo Shin <sto134679@gmail.com>
2024-05-13 19:58:03 +0900

    #116 Feat / Object LayerIdx 변경 기능 추가 1. 일단 소속된 레이어가 있는 경우에만 변경 가능

--- Project/Client/Inspector.cpp
+++ Project/Client/Inspector.cpp
@@ -1,6 +1,9 @@
 ﻿#include "pch.h"
 #include "Inspector.h"
 
+#include <Engine\CLevelMgr.h>
+#include <Engine\CLevel.h>
+#include <Engine\CLayer.h>
 #include <Engine/CTransform.h>
 
 #include "TransformUI.h"
@@ -38,6 +41,7 @@ void Inspector::render_update()
 
 	if (nullptr != m_TargetObject)
 	{
+		// 오브젝트 이름
 		char ObjName[255] = {};
 		string strName = string(m_TargetObject->GetName().begin(), m_TargetObject->GetName().end());
 		string prevName = strName;
@@ -49,11 +53,50 @@ void Inspector::render_update()
 
 		ImGui::SameLine();
 		ImGui::InputText("##ObjName", ObjName, 255);
-		GetTargetObject()->SetName(ToWString(string(ObjName)));
+		m_TargetObject->SetName(ToWString(string(ObjName)));
 
 		if (prevName != string(ObjName))
 		{
 			GamePlayStatic::ChangeName();
+		}
+
+		// 오브젝트 레이어
+		int LayerIdx = m_TargetObject->GetLayerIdx();
+		int PrevIdx = LayerIdx;
+
+		if (-1 != LayerIdx)
+		{
+			ImGui::Text("Layer"); ImGui::SameLine();
+			auto Layer_Names = magic_enum::enum_names<LAYER>();
+			string strLayer = string(Layer_Names[LayerIdx]);
+
+			if (ImGui::BeginCombo("##ObjLayer", strLayer.c_str()))
+			{
+				for (int i = 0; i < (int)Layer_Names.size(); ++i)
+				{
+					int CurLayer = i;
+
+					bool isSelected = (CurLayer == LayerIdx);
+
+					if (ImGui::Selectable(string(Layer_Names[CurLayer]).c_str(), isSelected))
+					{
+						LayerIdx = CurLayer;
+					}
+
+					if (isSelected)
+					{
+						ImGui::SetItemDefaultFocus();
+					}
+				}
+
+				ImGui::EndCombo();
+
+				if (PrevIdx != LayerIdx)
+				{
+					CLevel* pCurLevel = CLevelMgr::GetInst()->GetCurrentLevel();
+					pCurLevel->AddObject(m_TargetObject, LayerIdx);
+				}
+			}
 		}
 	}
 }
