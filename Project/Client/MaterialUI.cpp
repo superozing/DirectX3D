ae87d89d503a94f0a854a8112fffd0dcebf99a3a
superozing <amazingozing@gmail.com>
2024-05-14 23:24:35 +0900

    #114 Refactor / Engine Asset도 material의 이름이 뜨도록 수정
    Engine Asset은 파일 경로가 없기 때문에 이름이 설정되지 않았는데,
    키 값을 가져와서 Engine Asset도 이름을 표시하도록 수정했어요.

--- Project/Client/MaterialUI.cpp
+++ Project/Client/MaterialUI.cpp
@@ -24,8 +24,9 @@ void MaterialUI::render_update()
 
     // 해당 텍스쳐 이미지 출력
     Ptr<CMaterial> pMtrl = (CMaterial*)GetAsset().Get();
-    string strPath = string(pMtrl->GetRelativePath().begin(), pMtrl->GetRelativePath().end());
 
+    string strPath = string(pMtrl->GetKey().begin(), pMtrl->GetKey().end());
+    
     ImGui::Text("Material");
     ImGui::SameLine();
     ImGui::InputText("##TexName", (char*)strPath.c_str(), strPath.length(), ImGuiInputTextFlags_ReadOnly);
