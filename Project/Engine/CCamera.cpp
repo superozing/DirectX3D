378dbbdf1f70bd7c9a91986db5826e66f09f00f9
SEO DONG YUN <skdy1201@gmail.com>
2024-05-16 22:48:34 +0900

    #8 1. Frustum 구현 중   - 월드 공간상의 절두체 8개의 정점 위치를 기반으로 시야범위를 구성하는 6개의 평면 제작   - 만들어진 평면으로 특정 좌표가 절두체 내부에 있는지 체크해야 함

--- Project/Engine/CCamera.cpp
+++ Project/Engine/CCamera.cpp
@@ -33,6 +33,23 @@ CCamera::CCamera()
 	m_AspectRatio = vResol.x / vResol.y;
 	m_Width = vResol.x;
 	SetShake();
+
+	m_Frustum.SetOwner(this);
+}
+
+CCamera::CCamera(const CCamera& _Other)
+	: CComponent(_Other)
+	, m_Frustum(_Other.m_Frustum)
+	, m_ProjType(_Other.m_ProjType)
+	, m_FOV(_Other.m_FOV)
+	, m_Width(_Other.m_Width)
+	, m_Scale(_Other.m_Scale)
+	, m_AspectRatio(_Other.m_AspectRatio)
+	, m_Far(_Other.m_Far)
+	, m_LayerCheck(_Other.m_LayerCheck)
+	, m_CameraPriority(-1)
+{
+	m_Frustum.SetOwner(this);
 }
 
 CCamera::~CCamera()
@@ -126,6 +143,8 @@ void CCamera::finaltick()
 	}
 
 	m_matProjInv = XMMatrixInverse(nullptr, m_matProj);
+
+	m_Frustum.finaltick();
 }
 
 void CCamera::SetCameraPriority(int _Priority)
---
+++ Project/Engine/CFrustum.cpp
@@ -0,0 +1,61 @@
+﻿#include "pch.h"
+#include "CFrustum.h"
+
+#include "CCamera.h"
+
+CFrustum::CFrustum()
+    : m_Owner(nullptr)
+    , m_ProjPos{}
+    , m_arrFace{}
+{
+	// 투영좌표계 초기 좌표 설정
+    //     4 -- 5
+    //   / |   /|
+    //  /  7 -/ 6
+    // 0 -- 1  /
+    // | /  | /
+    // 3 -- 2
+
+    m_ProjPos[0] = Vec3(-1.f, 1.f, 0.f);
+    m_ProjPos[1] = Vec3(1.f, 1.f, 0.f);
+    m_ProjPos[2] = Vec3(1.f, -1.f, 0.f);
+    m_ProjPos[3] = Vec3(-1.f, -1.f, 0.f);
+
+    m_ProjPos[4] = Vec3(-1.f, 1.f, 1.f);
+    m_ProjPos[5] = Vec3(1.f, 1.f, 1.f);
+    m_ProjPos[6] = Vec3(1.f, -1.f, 1.f);
+    m_ProjPos[7] = Vec3(-1.f, -1.f, 1.f);
+
+}
+
+CFrustum::~CFrustum()
+{
+}
+
+
+void CFrustum::finaltick()
+{
+    Matrix matInv = m_Owner->GetProjInvMat() * m_Owner->GetViewInvMat();
+
+    Matrix viewinv = m_Owner->GetViewInvMat();
+    Matrix projinv = m_Owner->GetProjInvMat();
+
+
+    Vec3 vWorld[8] = {};
+    for (int i = 0; i < 8; ++i)
+    {
+        vWorld[i] = XMVector3TransformCoord(m_ProjPos[i], matInv);
+    }
+
+    if (m_Owner->GetOwner()->GetName() == L"Editor Camera");
+    {
+        int a = 0;
+    }
+
+    // WorldSpace 상에서 카메라의 시야를 표현하는 6 개의 평면 제작
+}
+
+bool CFrustum::FrustumCheck(Vec3 _vWorldPos)
+{
+    return false;
+}
