a127476018cdeb6c02f6af44324f21e0dd9a0d9d
jeontigger <boy9609@naver.com>
2024-05-17 17:26:33 +0900

    #111 Refactor / Manipulate(Gizmo)UI 위치, 컨텐츠 조정

--- Project/Client/RTViewPort.cpp
+++ Project/Client/RTViewPort.cpp
@@ -44,6 +44,7 @@ void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bo
     static bool boundSizing = false;
     static bool boundSizingSnap = false;
     static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
+
     if (editTransformDecomposition)
     {
         if (ImGui::IsKeyPressed(ImGuiKey_T))
@@ -61,15 +62,9 @@ void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bo
         if (ImGui::RadioButton("Scale", mCurrentGizmoOperation == ImGuizmo::SCALE))
             mCurrentGizmoOperation = ImGuizmo::SCALE;
 
-        float matrixTranslation[3], matrixRotation[3], matrixScale[3];
-        ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation, matrixRotation, matrixScale);
-        ImGui::InputFloat3("Tr", matrixTranslation);
-        ImGui::InputFloat3("Rt", matrixRotation);
-        ImGui::InputFloat3("Sc", matrixScale);
-        ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation, matrixScale, matrix);
-
         if (mCurrentGizmoOperation != ImGuizmo::SCALE)
         {
+            ImGui::SameLine();
             if (ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL))
                 mCurrentGizmoMode = ImGuizmo::LOCAL;
             ImGui::SameLine();
@@ -93,68 +88,26 @@ void EditTransform(float* cameraView, float* cameraProjection, float* matrix, bo
             ImGui::InputFloat("Scale Snap", &snap[0]);
             break;
         }
-        ImGui::Checkbox("Bound Sizing", &boundSizing);
-        if (boundSizing)
-        {
-            ImGui::PushID(3);
-            ImGui::Checkbox("##BoundSizing", &boundSizingSnap);
-            ImGui::SameLine();
-            ImGui::InputFloat3("Snap", boundsSnap);
-            ImGui::PopID();
-        }
     }
 
     ImGuiIO& io = ImGui::GetIO();
     float viewManipulateRight = ImGui::GetWindowSize().x;
     float viewManipulateTop = ImGui::GetWindowPos().y;
     static ImGuiWindowFlags gizmoWindowFlags = 0;
-    static bool useWindow = false;
-    if (useWindow)
-    {
-        ImGui::SetNextWindowSize(ImVec2(800, 400), ImGuiCond_Appearing);
-        ImGui::SetNextWindowPos(ImVec2(400, 20), ImGuiCond_Appearing);
-        ImGui::PushStyleColor(ImGuiCol_WindowBg, (ImVec4)ImColor(0.35f, 0.3f, 0.3f));
-        ImGui::Begin("Gizmo", 0, gizmoWindowFlags);
-        ImGuizmo::SetDrawlist();
-        float windowWidth = (float)ImGui::GetWindowWidth();
-        float windowHeight = (float)ImGui::GetWindowHeight();
-        ImGuizmo::SetRect(ImGui::GetWindowPos().x, ImGui::GetWindowPos().y, windowWidth, windowHeight);
-        viewManipulateRight = ImGui::GetWindowPos().x + windowWidth;
-        viewManipulateTop = ImGui::GetWindowPos().y;
-        //ImGuiWindow* window = ImGui::GetCurrentWindow();
-        //gizmoWindowFlags = ImGui::IsWindowHovered() && ImGui::IsMouseHoveringRect(window->InnerRect.Min, window->InnerRect.Max) ? ImGuiWindowFlags_NoMove : 0;
-    }
-    else
-    {
-        //ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
-        auto size = ImGui::GetWindowSize();
-        ImGuizmo::SetRect(0, 0, size.x, size.y);
 
-    }
-    ImGuizmo::Enable(true);
+    auto size = ImGui::GetWindowSize();
+    ImGuizmo::SetRect(0, 0, size.x, size.y);
 
     ImGuizmo::SetDrawlist();
-    //ImGuizmo::DrawGrid(cameraView, cameraProjection, identityMatrix, 100.f);
-    //ImGuizmo::DrawCubes(cameraView, cameraProjection, &objectMatrix[0][0], gizmoCount);
     ImGuizmo::Manipulate(cameraView, cameraProjection, mCurrentGizmoOperation, mCurrentGizmoMode, matrix, NULL, useSnap ? &snap[0] : NULL, boundSizing ? bounds : NULL, boundSizingSnap ? boundsSnap : NULL);
-
     //ImGuizmo::ViewManipulate(cameraView, _distance, ImVec2(viewManipulateRight - 128, viewManipulateTop), ImVec2(128, 128), 0x10101010);
-    auto pos = ImGuizmo::IsOver();
-    //ImGuizmo::
-    if (pos) {
-        int a = 0;
-    }
-    if (useWindow)
-    {
-        ImGui::End();
-        ImGui::PopStyleColor(1);
-    }
 }
 
 void RTViewPort::render_update()
 {
 	CRenderMgr::GetInst()->CopyRTTex(m_ViewPortTexture);
 
+    auto viewportPos = ImGui::GetWindowPos();
 	ImGui::Dummy(ImVec2(ImGui::GetWindowSize().x, ImGui::GetWindowSize().y - 40));
 
 	// 레벨 파일 드랍 체크
@@ -176,6 +129,7 @@ void RTViewPort::render_update()
 		ImGui::EndDragDropTarget();
 	}
 
+    
     ImGui::GetWindowDrawList()->AddImage(
         m_ViewPortTexture->GetSRV().Get(),
         ImVec2(0, 0),
@@ -195,42 +149,9 @@ void RTViewPort::render_update()
     ImGuiIO& io = ImGui::GetIO();
     
     ImGuizmo::SetOrthographic(false);
-    //ImGuizmo::BeginFrame();
     ImGuizmo::SetDrawlist();
 
-    ImGui::SetNextWindowPos(ImVec2(1024, 100), ImGuiCond_Appearing);
-    ImGui::SetNextWindowSize(ImVec2(256, 256), ImGuiCond_Appearing);
-
-    // create a window and insert the inspector
-    ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_Appearing);
-    ImGui::SetNextWindowSize(ImVec2(320, 340), ImGuiCond_Appearing);
-    ImGui::Begin("Editor");
-
-
-    ImGui::Text("Camera");
-    static int gizmoCount = 1;
-    ImGui::SliderInt("Gizmo count", &gizmoCount, 1, 4);
-
-
-    ImGui::Text("X: %f Y: %f", io.MousePos.x, io.MousePos.y);
-    ImGuizmo::Enable(true);
-    if (ImGuizmo::IsUsing())
-    {
-        ImGui::Text("Using gizmo");
-    }
-    else
-    {
-        ImGui::Text(ImGuizmo::IsOver() ? "Over gizmo" : "");
-        ImGui::SameLine();
-        ImGui::Text(ImGuizmo::IsOver(ImGuizmo::TRANSLATE) ? "Over translate gizmo" : "");
-        ImGui::SameLine();
-        ImGui::Text(ImGuizmo::IsOver(ImGuizmo::ROTATE) ? "Over rotate gizmo" : "");
-        ImGui::SameLine();
-        ImGui::Text(ImGuizmo::IsOver(ImGuizmo::SCALE) ? "Over scale gizmo" : "");
-    }
-    ImGui::Separator();
-
-    ImGui::End();
+    ImGui::SetCursorPos(ImVec2(10, 30));
     auto cameraViewMat = g_Transform.matView;
     auto cameraProjMat = g_Transform.matProj;
     auto player = CLevelMgr::GetInst()->GetCurrentLevel()->FindObjectByName(L"Player");
