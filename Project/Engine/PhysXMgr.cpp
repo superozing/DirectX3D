77f16fd1f419fa9be02a7480463ea1a9eda0e2db
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-06-12 17:47:14 +0900

    #158 PhysXMgr 작성

---
+++ Project/Engine/PhysXMgr.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "PhysXMgr.h"
+#include "CTimeMgr.h"
+
+void PhysXMgr::init()
+{
+    // PhysX 초기화
+    gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+    gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true);
+    PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+    sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+    gDispatcher = PxDefaultCpuDispatcherCreate(2);
+    sceneDesc.cpuDispatcher = gDispatcher;
+    sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+    gScene = gPhysics->createScene(sceneDesc);
+}
+
+void PhysXMgr::tick()
+{
+    gScene->simulate(DT);
+    gScene->fetchResults(true);
+}
+
+PhysXMgr::~PhysXMgr()
+{
+    gScene->release();
+    gDispatcher->release();
+    gPhysics->release();
+    gFoundation->release();
+}
+
---
+++ Project/Engine/PhysXMgr.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "CManager.h"
+#include "physx/PxPhysicsAPI.h"
+
+using namespace physx;
+
+class PhysXMgr :
+    public CManager<PhysXMgr>
+{
+	SINGLE(PhysXMgr);
+
+private:
+	PxDefaultAllocator      gAllocator;
+	PxDefaultErrorCallback  gErrorCallback;
+	PxFoundation* gFoundation = nullptr;
+	PxPhysics* gPhysics = nullptr;
+	PxDefaultCpuDispatcher* gDispatcher = nullptr;
+	PxScene* gScene = nullptr;
+
+public:
+	virtual void init() override;
+	virtual void tick() override;
+	virtual void enter() override {}
+
+private:
+	void Clear() {};
+};
