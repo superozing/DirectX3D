e415db84fa16deb27ae473d407d7bacb360849e5
jeontigger <boy9609@naver.com>
2024-06-27 15:36:57 +0900

    #185 Feat / 플레이어 노말, 사격 움직임 구현 1. 노말 움직임은 카메라 방향으로 선형회전 2. 사격 움직임은 회전 없이 움직임

--- Project/Scripts/CPlayerScript_State.cpp
+++ Project/Scripts/CPlayerScript_State.cpp
@@ -2,36 +2,51 @@
 #include "CPlayerScript.h"
 
 #include "CRoRStateMachine.h"
+#include <Engine/CRenderMgr.h>
 
 void CPlayerScript::NormalBegin()
 {
 	// 노말 애니메이션 시작
+	Animator3D()->Play(2);
 }
 
 int CPlayerScript::NormalUpdate()
 {
-	// TODO: 마우스 x축 회전에 따라 모델의 y축이 회전해야 함
+	// TODO: 마우스 x축 회전에 따라 스프링암의 y축이 회전해야 함
 	// TODO: 키 입력 커스터마이징 가능하도록 민서 Controller 완성되면 붙여야 함
 
-	// 카메라에 따라 8방향 움직임
-	Vec3 vPos	= Transform()->GetRelativePos();
-	Vec3 vFront = Transform()->GetWorldDir(DIR_TYPE::FRONT);
-	Vec3 vRight = Transform()->GetWorldDir(DIR_TYPE::RIGHT);
+	// 카메라에 따라 캐릭터 8방향 움직임 - 스프링 암에서 가져와야 함
+	// 마지막 키 입력 추적해야 함 - 앞, 뒤랑 양 옆을
+	CGameObject* pCamera   = CRenderMgr::GetInst()->GetMainCam()->GetOwner();
+	Vec3		 vCamFront = pCamera->Transform()->GetWorldDir(DIR_TYPE::FRONT);
+	Vec3		 vCamRight = pCamera->Transform()->GetWorldDir(DIR_TYPE::RIGHT);
+	Vec3		 vTargetDir;
 	if (KEY_PRESSED(W))
 	{
-		vPos += vFront * m_Speed * DT;
+		vTargetDir += vCamFront;
 	}
 	if (KEY_PRESSED(S))
 	{
-		vPos -= vFront * m_Speed * DT;
+		vTargetDir += -vCamFront;
 	}
 	if (KEY_PRESSED(D))
 	{
-		vPos += vRight * m_Speed * DT;
+		vTargetDir += vCamRight;
 	}
 	if (KEY_PRESSED(A))
 	{
-		vPos -= vRight * m_Speed * DT;
+		vTargetDir += -vCamRight;
+	}
+
+	Vec3 vPos	= Transform()->GetRelativePos();
+	Vec3 vFront = Transform()->GetWorldDir(DIR_TYPE::FRONT);
+	Vec3 vRight = Transform()->GetWorldDir(DIR_TYPE::RIGHT);
+	if (vTargetDir != Vec3())
+	{
+		vPos += vFront * m_Speed * DT;
+		float fAngleSpeed = 20.f;
+		vTargetDir		  = Vec3::Lerp(Transform()->GetWorldDir(DIR_TYPE::FRONT), vTargetDir, DT * fAngleSpeed);
+		Transform()->SetDir(vTargetDir);
 	}
 
 	Transform()->SetRelativePos(vPos);
@@ -57,6 +72,30 @@ void CPlayerScript::AttackBegin()
 
 int CPlayerScript::AttackUpdate()
 {
+	// 기본 움직임
+	Vec3 vPos	= Transform()->GetRelativePos();
+	Vec3 vFront = Transform()->GetWorldDir(DIR_TYPE::FRONT);
+	Vec3 vRight = Transform()->GetWorldDir(DIR_TYPE::RIGHT);
+
+	if (KEY_PRESSED(W))
+	{
+		vPos += vFront * m_Speed * DT;
+	}
+	if (KEY_PRESSED(S))
+	{
+		vPos += -vFront * m_Speed * DT;
+	}
+	if (KEY_PRESSED(D))
+	{
+		vPos += vRight * m_Speed * DT;
+	}
+	if (KEY_PRESSED(A))
+	{
+		vPos += -vRight * m_Speed * DT;
+	}
+
+	Transform()->SetRelativePos(vPos);
+
 	if (KEY_TAP(KEY::SPACE))
 	{
 		return m_FSM->GetCurState() + 1;
