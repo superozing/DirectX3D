319c199a80c3a705288febafdb5f0a762ae1ce60
jeontigger <boy9609@naver.com>
2024-07-01 15:11:10 +0900

    #152 Refactor / 스프링 암 위치 조정 기능 가능하도록 구조 변경

--- Project/Scripts/CSpringArm.cpp
+++ Project/Scripts/CSpringArm.cpp
@@ -5,21 +5,17 @@
 
 CSpringArm::CSpringArm()
 	: CScript((UINT)SCRIPT_TYPE::SPRINGARM)
-	, m_fDistance(300.f)
-	, m_vDir(1.f, 0.f, 0.f)
-	, m_vOffset()
-	, m_bActive(true)
-	, m_bType(true)
-	, m_fCamSpeed(20.f)
+	, m_tInfo{}
+
 {
 	AppendScriptParam("Active", SCRIPT_PARAM::BOOL, &m_bActive);
-	AppendScriptParam("Smooth", SCRIPT_PARAM::BOOL, &m_bType);
-	AppendScriptParam("Distance", SCRIPT_PARAM::FLOAT, &m_fDistance);
-	AppendScriptParam("Speed", SCRIPT_PARAM::FLOAT, &m_fCamSpeed);
-	AppendScriptParam("RotSpeed", SCRIPT_PARAM::FLOAT, &m_fCamRotateSpeed);
-	AppendScriptParam("TargetPos", SCRIPT_PARAM::VEC3, &m_vTargetPos);
-	AppendScriptParam("Offset", SCRIPT_PARAM::VEC3, &m_vOffset);
-	AppendScriptParam("Direction", SCRIPT_PARAM::VEC3, &m_vDir);
+	AppendScriptParam("Smooth", SCRIPT_PARAM::BOOL, &m_tInfo.Type);
+	AppendScriptParam("Distance", SCRIPT_PARAM::FLOAT, &m_tInfo.fDistance);
+	AppendScriptParam("Speed", SCRIPT_PARAM::FLOAT, &m_tInfo.fCamSpeed);
+	AppendScriptParam("RotSpeed", SCRIPT_PARAM::FLOAT, &m_tInfo.fCamRotSpeed);
+	AppendScriptParam("PosOffset", SCRIPT_PARAM::VEC3, &m_tInfo.vOffset);
+	AppendScriptParam("DirOffset", SCRIPT_PARAM::VEC3, &m_tInfo.vDirOffset);
+	AppendScriptParam("Direction", SCRIPT_PARAM::VEC3, &m_tInfo.vDir);
 	AppendScriptObject("Cam", &m_pTargetCam, COMPONENT_TYPE::CAMERA);
 }
 
@@ -31,7 +27,6 @@ void CSpringArm::begin()
 {
 	auto cam = CRenderMgr::GetInst()->GetMainCam();
 	SetTargetCamera(cam);
-	m_vTargetPos = Transform()->GetWorldPos() + m_vOffset + m_vDir.Normalize() * m_fDistance;
 }
 
 void CSpringArm::tick()
@@ -39,48 +34,48 @@ void CSpringArm::tick()
 	if (!GetOwner() || !Transform() || !m_pTargetCam || !m_bActive)
 		return;
 
-	static float fCurDistance = m_fDistance;
-	static Vec3	 vCurDir	  = m_vDir;
-	static Vec3	 vCurPos	  = m_vTargetPos;
+	static float fCurDistance = m_tInfo.fDistance;
 
 	// 테스트 코드
 	// 머지 후 playerscript의 바깥에서 해야하는 역할
 	if (KEY_PRESSED(H))
 	{
-		m_vDir = m_vDir.RotateAround(Vec3(0.f, 1.f, 0.f), 1.f * DT);
+		m_tInfo.vDir = m_tInfo.vDir.RotateAround(Vec3(0.f, 1.f, 0.f), 1.f * DT);
 	}
 	if (KEY_PRESSED(G))
 	{
-		m_vDir = m_vDir.RotateAround(Vec3(0.f, 1.f, 0.f), -1.f * DT);
+		m_tInfo.vDir = m_tInfo.vDir.RotateAround(Vec3(0.f, 1.f, 0.f), -1.f * DT);
 	}
 
 	if (KEY_TAP(J))
 	{
-		m_fDistance = 100.f;
+		m_tInfo.fDistance = 100.f;
 	}
 	if (KEY_TAP(K))
 	{
-		m_fDistance = 300.f;
+		m_tInfo.fDistance = 300.f;
 	}
 	// 테스트 코드 끝
 
-	Vec3 vNewPos = GetOwner()->Transform()->GetWorldPos();
-	vNewPos += m_vDir.Normalize() * fCurDistance + m_vOffset;
-	Vec3 vNewDir = (GetOwner()->Transform()->GetWorldPos() + m_vOffset) - m_pTargetCam->Transform()->GetWorldPos();
+	Vec3 vNewPos =
+		(GetOwner()->Transform()->GetWorldPos() + m_tInfo.vOffset) + m_tInfo.vDir.Normalize() * m_tInfo.fDistance;
+	Vec3 vNewDir = (GetOwner()->Transform()->GetWorldPos() + m_tInfo.vOffset + m_tInfo.vDirOffset) -
+				   m_pTargetCam->Transform()->GetWorldPos();
 
 	// false : Lerp, true : Smooth
-	if (m_bType)
+	if (m_tInfo.Type)
 	{
-		vNewPos = Vec3::SmoothStep(m_pTargetCam->Transform()->GetWorldPos(), vNewPos, DT * m_fCamSpeed);
-		vNewDir =
-			Vec3::SmoothStep(m_pTargetCam->Transform()->GetWorldDir(DIR_TYPE::FRONT), vNewDir, DT * m_fCamRotateSpeed);
-		fCurDistance = RoRMath::SmoothStep(fCurDistance, m_fDistance, DT * m_fCamSpeed);
+		vNewPos		 = Vec3::SmoothStep(m_pTargetCam->Transform()->GetWorldPos(), vNewPos, DT * m_tInfo.fCamSpeed);
+		vNewDir		 = Vec3::SmoothStep(m_pTargetCam->Transform()->GetWorldDir(DIR_TYPE::FRONT), vNewDir,
+										DT * m_tInfo.fCamRotSpeed);
+		fCurDistance = RoRMath::SmoothStep(fCurDistance, m_tInfo.fDistance, DT * m_tInfo.fCamSpeed);
 	}
 	else
 	{
-		vNewPos = Vec3::Lerp(m_pTargetCam->Transform()->GetWorldPos(), vNewPos, DT * m_fCamSpeed);
-		vNewDir = Vec3::Lerp(m_pTargetCam->Transform()->GetWorldDir(DIR_TYPE::FRONT), vNewDir, DT * m_fCamRotateSpeed);
-		fCurDistance = RoRMath::Lerp(fCurDistance, m_fDistance, DT * m_fCamSpeed);
+		vNewPos = Vec3::Lerp(m_pTargetCam->Transform()->GetWorldPos(), vNewPos, DT * m_tInfo.fCamSpeed);
+		vNewDir =
+			Vec3::Lerp(m_pTargetCam->Transform()->GetWorldDir(DIR_TYPE::FRONT), vNewDir, DT * m_tInfo.fCamRotSpeed);
+		fCurDistance = RoRMath::Lerp(fCurDistance, m_tInfo.fDistance, DT * m_tInfo.fCamSpeed);
 	}
 
 	m_pTargetCam->Transform()->SetRelativePos(vNewPos);
--- Project/Scripts/CSpringArm.h
+++ Project/Scripts/CSpringArm.h
@@ -1,55 +1,59 @@
 ﻿#pragma once
 #include <Engine\CScript.h>
 
+struct SpringArmInfo
+{
+	/// @brief 스프링 암이 향하는 각도
+	Vec3 vDir = Vec3(0.f, 1.f, 1.f);
+	/// @brief 부모 객체의 포지션에서 시작지점을 변경하는 오프셋
+	Vec3 vOffset;
+	/// @brief 카메라가 바라보는 지점을 시작지점으로부터 돌려주는 오프셋
+	Vec3 vDirOffset;
+	/// @brief 스프링 암의 길이
+	float fDistance = 500.f;
+	/// @brief 카메라의 위치를 이동시키는 속도입니다.
+	float fCamSpeed = 20.f;
+	/// @brief 카메라의 물체를 바라보는 속도 입니다.
+	float fCamRotSpeed = 20.f;
+	/// @brief false : Lerp, true : Smooth
+	bool Type = true;
+};
+
 class CSpringArm : public CScript
 {
 private:
 	/// @brief 스프링 암에 매달리는 카메라
 	CGameObject* m_pTargetCam;
-
-	/// @brief 스프링 암이 추적하는 위치
-	Vec3 m_vTargetPos;
-	/// @brief 스프링 암이 향하는 각도
-	Vec3 m_vDir;
-	/// @brief 부모 객체의 포지션에서 시작지점을 변경하는 오프셋
-	Vec3 m_vOffset;
-	/// @brief 카메라가 바라보는 지점을 시작지점으로부터 돌려주는 오프셋
-	// Vec3 m_vDirOffset;
-	/// @brief 스프링 암의 길이
-	float m_fDistance;
-
-	/// @brief 카메라의 위치를 이동시키는 속도입니다.
-	float m_fCamSpeed;
-	/// @brief 카메라의 물체를 바라보는 속도 입니다.
-	float m_fCamRotateSpeed;
-
 	/// @brief 활성화 여부
-	bool m_bActive;
-	/// @brief false : Lerp, true : Smooth
-	bool m_bType;
+	bool m_bActive = true;
+
+	SpringArmInfo m_tInfo;
 
 public:
 	/// @brief 스프링 암에 붙는 카메라를 세팅하는 함수입니다.
 	void SetTargetCamera(CCamera* _cam);
 	/// @brief 스프링 암이 시작하는 지점부터 캠으로 향하는 방향을 세팅하는 함수입니다.
-	void SetDir(Vec3 _dir) { m_vDir = _dir; }
+	void SetDir(Vec3 _dir) { m_tInfo.vDir = _dir; }
 	/// @brief 스프링 암이 시작되는 지점을 지정하는 함수입니다.
-	void SetOffsetPos(Vec3 _vOffset) { m_vOffset = _vOffset; }
+	void SetOffsetPos(Vec3 _vOffset) { m_tInfo.vOffset = _vOffset; }
 	/// @brief 스프링 암에 붙어있는 카메라가 바라보는 지점을 돌려주는 오프셋을 지정하는 함수입니다.
-	// void SetOffsetDir(Vec3 _vOffset) { m_vDirOffset = _vOffset; }
+	void SetOffsetDir(Vec3 _vOffset) { m_tInfo.vDirOffset = _vOffset; }
 	/// @brief 스프링 암의 길이를 지정하는 함수입니다.
-	void SetDistance(float _dist) { m_fDistance = _dist; }
+	void SetDistance(float _dist) { m_tInfo.fDistance = _dist; }
 
 	/// @brief 캠이 따라붙는 속도를 설정하는 함수입니다.
-	void SetCamSpeed(float _speed) { m_fCamSpeed = _speed; }
+	void SetCamSpeed(float _speed) { m_tInfo.fCamSpeed = _speed; }
 	/// @brief 캠이 시작지점으로 회전하는 속도를 설정하는 함수입니다.
-	void SetCamRotSpeed(float _speed) { m_fCamRotateSpeed = _speed; }
+	void SetCamRotSpeed(float _speed) { m_tInfo.fCamRotSpeed = _speed; }
 
 	/// @brief 스프링 암의 활성화 여부를 결정하는 함수입니다.
 	void Activate(bool _active) { m_bActive = _active; }
 	/// @brief 카메라의 이동을 Smooth로 할지, Lerp로 할지 결정하는 함수입니다.
 	/// @param _smooth true : Smooth, false : Lerp
-	void SetMoveType(bool _type) { m_bType = _type; }
+	void SetMoveType(bool _type) { m_tInfo.Type = _type; }
+
+	SpringArmInfo GetInfo() { return m_tInfo; }
+	void		  SetInfo(SpringArmInfo _info) { m_tInfo = _info; }
 
 public:
 	virtual void begin() override;
