f4e15c09e75774f378dba79a010d422b5073fa2d
superozing <amazingozing@gmail.com>
2024-07-04 17:37:52 +0900

    #186 Feat / CAmmoInfo 추가
    무기 UI에 들어갈 프로그래스 바 파생 클래스

---
+++ Project/Scripts/CAmmoInfo.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "CProgressBar.h"
+
+class CAmmoInfo : public CProgressBar
+{
+private:
+	// HP Line UI
+	CImageUIScript* m_pAmmoLine;
+
+public:
+	virtual void begin() override;
+	virtual void tick() override;
+
+private:
+	virtual void MakeChildObjects() override;
+
+public:
+	void SetMaxAmmo(int _MaxHP) { CProgressBar::SetMaxValue(RoRMath::ClampInt(_MaxHP, 0)); }
+	void SetCurAmmo(int _CurHP)
+	{
+		CProgressBar::SetCurValue(RoRMath::ClampInt(_CurHP, 0, CProgressBar::GetMaxValue()));
+	}
+
+	int GetMaxAmmo() const { return CProgressBar::GetMaxValue(); }
+	int GetCurAmmo() const { return CProgressBar::GetCurValue(); }
+
+public:
+	CLONE(CAmmoInfo);
+	CAmmoInfo();
+	~CAmmoInfo();
+};
