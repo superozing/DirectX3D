134bc368d1bd1caee5cae18b522338db5f9748ac
superozing <amazingozing@gmail.com>
2024-07-08 06:24:53 +0900

    #192 Feat / PlayerHP 기능 추가
    1. 기존 3번 그리던 드로우콜을 쉐이더 코드를 사용해서 한 번으로 줄임
    2. HP 바 적용

--- Project/Scripts/CPlayerHP.cpp
+++ Project/Scripts/CPlayerHP.cpp
@@ -21,15 +21,22 @@ void CPlayerHP::begin()
 
 void CPlayerHP::tick()
 {
+	int CurHP = GetCurHP();
+	int MaxHP = GetMaxHP();
+
+	if (MaxHP)
+		m_pHPLineUI->MeshRender()->GetMaterial(0)->SetScalarParam(SCALAR_PARAM::FLOAT_1, (float)CurHP / MaxHP);
 }
 
 void CPlayerHP::MakeChildObjects()
 {
 	// transform 조정
-	Transform()->SetRelativeScale(Vec3(361.f, 55.f, 1.f));
+	Transform()->SetRelativeScale(Vec3(380.f, 55.f, 1.f));
 
 	// panel texture 설정
-	GetPanelUI()->SetPanelTex(CAssetMgr::GetInst()->Load<CTexture>(L"texture/ui/HPLine_Bg_Middle.png"));
+	GetPanelUI()->SetPanelTex(CAssetMgr::GetInst()->Load<CTexture>(L"texture/ui/HPLine_Bg.png"));
+	MeshRender()->GetMaterial(0)->SetScalarParam(SCALAR_PARAM::BOOL_0, true);
+	MeshRender()->GetMaterial(0)->SetScalarParam(SCALAR_PARAM::FLOAT_0, 0.9f);
 
 	CGameObject* pObj = nullptr;
 
@@ -44,52 +51,13 @@ void CPlayerHP::MakeChildObjects()
 	pObj->MeshRender()->SetMesh(CAssetMgr::GetInst()->FindAsset<CMesh>(MESHrect));
 	pObj->MeshRender()->SetMaterial(CAssetMgr::GetInst()->FindAsset<CMaterial>(L"StaticUIMtrl"), 0);
 	pObj->MeshRender()->GetDynamicMaterial(0);
+	pObj->MeshRender()->GetMaterial(0)->SetScalarParam(SCALAR_PARAM::BOOL_1, true);
 
 	m_pHPLineUI->SetUIImg(CAssetMgr::GetInst()->Load<CTexture>(L"texture/ui/HPLine.png"));
 	m_pHPLineUI->AllowBindTexPerFrame();
 
 	pObj->Transform()->SetRelativePos(Vec3(0, 0, 0.f));
 	pObj->Transform()->SetRelativeScale(Vec3(361, 35.f, 1.f));
-
-	GetOwner()->AddChild(pObj);
-
-	// Bg_L
-	pObj		= new CGameObject;
-	m_pHPLineUI = new CImageUIScript;
-	pObj->SetName(L"Bg_L");
-	pObj->AddComponent(new CTransform);
-	pObj->AddComponent(new CMeshRender);
-	pObj->AddComponent(m_pHPLineUI);
-
-	pObj->MeshRender()->SetMesh(CAssetMgr::GetInst()->FindAsset<CMesh>(MESHrect));
-	pObj->MeshRender()->SetMaterial(CAssetMgr::GetInst()->FindAsset<CMaterial>(L"StaticUIMtrl"), 0);
-	pObj->MeshRender()->GetDynamicMaterial(0);
-
-	m_pHPLineUI->SetUIImg(CAssetMgr::GetInst()->Load<CTexture>(L"texture/ui/HPLine_Bg_Left.png"));
-	m_pHPLineUI->AllowBindTexPerFrame();
-
-	pObj->Transform()->SetRelativePos(Vec3(-187, 0, -200.f));
-	pObj->Transform()->SetRelativeScale(Vec3(15, 55.f, 1.f));
-
-	GetOwner()->AddChild(pObj);
-
-	// Bg_R
-	pObj		= new CGameObject;
-	m_pHPLineUI = new CImageUIScript;
-	pObj->SetName(L"Bg_R");
-	pObj->AddComponent(new CTransform);
-	pObj->AddComponent(new CMeshRender);
-	pObj->AddComponent(m_pHPLineUI);
-
-	pObj->MeshRender()->SetMesh(CAssetMgr::GetInst()->FindAsset<CMesh>(MESHrect));
-	pObj->MeshRender()->SetMaterial(CAssetMgr::GetInst()->FindAsset<CMaterial>(L"StaticUIMtrl"), 0);
-	pObj->MeshRender()->GetDynamicMaterial(0);
-
-	m_pHPLineUI->SetUIImg(CAssetMgr::GetInst()->Load<CTexture>(L"texture/ui/HPLine_Bg_Right.png"));
-	m_pHPLineUI->AllowBindTexPerFrame();
-
-	pObj->Transform()->SetRelativePos(Vec3(187, 0, -200.f));
-	pObj->Transform()->SetRelativeScale(Vec3(15, 55.f, 1.f));
 
 	GetOwner()->AddChild(pObj);
 }
