25143c3d963f85ca3bbef2ba31acc5e377eca814
jeontigger <boy9609@naver.com>
2024-07-09 17:10:17 +0900

    HOTFIX / Escape 할 때와 아닐 때 키 분기 처리 수정사항 추가

--- Project/Client/CCreatePlayerTestLevel.cpp
+++ Project/Client/CCreatePlayerTestLevel.cpp
@@ -104,30 +104,6 @@ void CCreatePlayerTestLevel::CreateTempLevel()
 
 	pTempLevel->AddObject(pObj, (UINT)LAYER::LAYER_DEFAULT, false);
 
-	// Player Object 생성
-	pObj = new CGameObject;
-	pObj->SetName(L"Player");
-
-	pObj->AddComponent(new CTransform);
-	pObj->AddComponent(new CMeshRender);
-	pObj->AddComponent(new CPlayerScript);
-
-	pObj->Transform()->SetRelativePos(Vec3(0.f, -500.f, 500.f));
-	pObj->Transform()->SetRelativeScale(Vec3(5000.f, 5000.f, 500.f));
-	pObj->Transform()->SetRelativeRotation(Vec3(XM_PI / 2.f, 0.f, 0.f));
-
-	pObj->MeshRender()->SetMesh(CAssetMgr::GetInst()->FindAsset<CMesh>(MESHrect));
-
-	pObj->MeshRender()->SetMaterial(CAssetMgr::GetInst()->FindAsset<CMaterial>(L"Std3D_DeferredMtrl"), 0);
-	pObj->MeshRender()->GetMaterial(0)->SetTexParam(
-		TEX_PARAM::TEX_0,
-		CAssetMgr::GetInst()->Load<CTexture>(L"texture\\tile\\TILE_01.tga", L"texture\\tile\\TILE_01.tga"));
-	pObj->MeshRender()->GetMaterial(0)->SetTexParam(
-		TEX_PARAM::TEX_1,
-		CAssetMgr::GetInst()->Load<CTexture>(L"texture\\tile\\TILE_01_N.tga", L"texture\\tile\\TILE_01_N.tga"));
-
-	pTempLevel->AddObject(pObj, (UINT)LAYER::LAYER_PLAYER, false);
-
 	GamePlayStatic::ChangeLevel(pTempLevel, LEVEL_STATE::STOP);
 
 	// ============
