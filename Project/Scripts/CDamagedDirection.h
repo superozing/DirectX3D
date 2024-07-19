2a169f9056394b1f970ef90f81e3736b217049e0
superozing <amazingozing@gmail.com>
2024-07-19 17:34:10 +0900

    #221 Feat / 피격 방향 표시 UI 추가 중

---
+++ Project/Scripts/CDamagedDirection.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <Engine/CScript.h>
+
+class CImageUIScript;
+class CPanelUIScript;
+
+class CDamagedDirection : public CScript
+{
+private:
+	// 기반 패널UI
+	CPanelUIScript* m_pPanelUI;
+
+	// 방향 이미지
+	CImageUIScript* m_pImageUI;
+
+	// 최대 데미지를 입을 경우의 반지름
+	float m_fMaxDamageRadius;
+
+	float m_DamageRadiusRatio; 
+	
+	Vec3 m_DamagedDirection;
+
+public:
+	virtual void begin() override;
+	virtual void tick() override;
+
+	virtual void SaveToFile(FILE* _File) override;
+	virtual void SaveToFile(ofstream& fout) override;
+	virtual void LoadFromFile(FILE* _File) override;
+	virtual void LoadFromFile(ifstream& fin) override;
+
+public:
+	void SetDamagedDirection(Vec3 _DamagedDirection) { m_DamagedDirection = _DamagedDirection; }
+	void SetMaxDamageRadius(float _Angle) { m_fMaxDamageRadius = _Angle; }
+
+private:
+	void SetParentPanelUI();
+
+public:
+	CLONE(CDamagedDirection);
+	CDamagedDirection();
+	~CDamagedDirection();
+};
