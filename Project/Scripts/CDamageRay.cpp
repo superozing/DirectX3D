ffa7b878bab69b69db449bacd4ad992bda2e3514
SEO DONG YUN <skdy1201@gmail.com>
2024-08-08 08:10:14 +0900

    #253 임시 커밋

---
+++ Project/Scripts/CDamageRay.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include "CDamageRay.h"
+
+#include <Engine\CPhysXMgr.h>
+
+CDamageRay::CDamageRay()
+	: CScript((UINT)SCRIPT_TYPE::DAMAGERAY)
+{
+}
+
+CDamageRay::CDamageRay(const CDamageRay& _Other)
+	: CScript((UINT)_Other.GetScriptType())
+{
+}
+
+CDamageRay::~CDamageRay()
+{
+}
+
+void CDamageRay::begin()
+{
+}
+
+void CDamageRay::tick()
+{
+	tRoRHitInfo hitInfo = {};
+
+	CPhysXMgr::GetInst()->PerfomRaycast(GetOwner()->Transform()->GetWorldPos(),
+										GetOwner()->Transform()->GetWorldDir(DIR_TYPE::FRONT), hitInfo,
+										(UINT)LAYER::LAYER_PLAYER);
+}
+
+void CDamageRay::SaveToFile(FILE* _File)
+{
+}
+
+void CDamageRay::SaveToFile(ofstream& fout)
+{
+}
+
+void CDamageRay::LoadFromFile(FILE* _File)
+{
+}
+
+void CDamageRay::LoadFromFile(ifstream& fin)
+{
+}
---
+++ Project/Scripts/CDamageRay.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <Engine\CScript.h>
+
+class CDamageRay : public CScript
+{
+public:
+	virtual void begin() override;
+	virtual void tick() override;
+
+	virtual void SaveToFile(FILE* _File) override;
+	virtual void SaveToFile(ofstream& fout) override;
+	virtual void LoadFromFile(FILE* _File) override;
+	virtual void LoadFromFile(ifstream& fin) override;
+
+public:
+	CLONE(CDamageRay);
+	CDamageRay();
+	CDamageRay(const CDamageRay& _Other);
+	~CDamageRay();
+};
