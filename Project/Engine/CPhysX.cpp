b6411e29a436266b743bd9e259eb41d99f1aa16f
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-08-08 20:36:33 +0900

    디버그매쉬수정

--- Project/Engine/CPhysX.cpp
+++ Project/Engine/CPhysX.cpp
@@ -231,7 +231,7 @@ void CPhysX::finaltick()
 		{
 			GamePlayStatic::DrawDebugSphere(DebugFinalPos, m_vScale.x / 2.f, Vec3(0.3f, .3f, 0.3f), false);
 		}
-		else if (PhysShape::SPHERE == m_Shape)
+		else if (PhysShape::CONE == m_Shape)
 		{
 			GamePlayStatic::DrawDebugCone(DebugFinalPos, m_vScale, Vec4(Rot.x, Rot.y, Rot.z, Rot.w),
 										  Vec3(0.3f, .3f, 0.3f), false);
