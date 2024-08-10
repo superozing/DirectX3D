a44b12e01affd441c8f0da6fddc490f4746d3d5f
oljindudol <82567430+oljindudol@users.noreply.github.com>
2024-08-10 16:30:54 +0900

    회전이0일때의 밀어주기구현 완료

--- Project/Scripts/CCubePushScript.cpp
+++ Project/Scripts/CCubePushScript.cpp
@@ -1,5 +1,58 @@
 ﻿#include "pch.h"
 #include "CCubePushScript.h"
+#include "Engine\CPhysX.h"
+#include "Engine\CTransform.h"
+#include "Engine\CLogMgr.h"
+
+void CCubePushScript::resolveCollision(CGameObject* _Player)
+{
+	auto ThisColPos	  = PhysX()->GetColliderPos();
+	auto PlayerColPos = _Player->PhysX()->GetColliderPos();
+
+	auto ThisColScale = PhysX()->m_vScale;
+	auto PlayerScale  = _Player->PhysX()->m_vScale;
+
+	auto ThisColRot	  = Transform()->GetWorldRot();
+	auto PlayerColRot = _Player->Transform()->GetWorldRot();
+
+	// 밀어줄 Offset
+	Vec3 offset;
+	// ####################################################################################################
+	//  TODO : 밀어줄 오프셋을 계산
+	// 벽과 플레이어의 중심에서의 거리 계산
+	Vec3 toWall = ThisColPos - PlayerColPos;
+
+	// 간섭 거리 계산
+	float overlapX = (PlayerScale.x / 2 + ThisColScale.x / 2) - std::fabs(toWall.x);
+	float overlapY = (PlayerScale.y / 2 + ThisColScale.y / 2) - std::fabs(toWall.y);
+	float overlapZ = (PlayerScale.z / 2 + ThisColScale.z / 2) - std::fabs(toWall.z);
+
+	// 가장 큰 간섭 거리와 방향 선택
+	float overlap = min(min(overlapX, overlapY), overlapZ);
+
+	// 법선 방향 계산 (간섭이 가장 큰 방향)
+	Vec3 pushDirection;
+	if (std::fabs(overlapX) <= std::fabs(overlapY) && std::fabs(overlapX) <= std::fabs(overlapZ))
+	{
+		pushDirection = (toWall.x > 0) ? Vec3(-1, 0, 0) : Vec3(1, 0, 0); // X축 방향
+	}
+	else if (std::fabs(overlapY) <= std::fabs(overlapZ))
+	{
+		pushDirection = (toWall.y > 0) ? Vec3(0, -1, 0) : Vec3(0, 1, 0); // Y축 방향
+	}
+	else
+	{
+		pushDirection = (toWall.z > 0) ? Vec3(0, 0, -1) : Vec3(0, 0, 1); // Z축 방향
+	}
+
+	// 밀어줄 오프셋 계산 (간섭 거리와 방향을 고려)
+	offset = pushDirection * overlap;
+	CLogMgr::GetInst()->AddLog(Log_Level::INFO, offset.ToString());
+	// ####################################################################################################
+
+	auto PlayerPos = _Player->Transform()->GetRelativePos();
+	_Player->Transform()->SetRelativePos(PlayerPos + offset);
+}
 
 void CCubePushScript::begin()
 {
@@ -9,8 +62,7 @@ void CCubePushScript::tick()
 {
 }
 
-#include "Engine\CPhysX.h"
-void CCubePushScript::Overlap(CCollider2D* _Collider, CGameObject* _OtherObj, CCollider2D* _OtherCollider)
+void CCubePushScript::Overlap(CPhysX* _Collider, CGameObject* _OtherObj, CPhysX* _OtherCollider)
 {
 	bool bIsPlayer = false;
 
@@ -25,9 +77,7 @@ void CCubePushScript::Overlap(CCollider2D* _Collider, CGameObject* _OtherObj, CC
 	}
 	if (false == bIsPlayer)
 		return;
-
-	auto ThisColPos	  = PhysX()->GetColliderPos();
-	auto PlayerColPos = _OtherObj->PhysX()->GetColliderPos();
+	resolveCollision(_OtherObj);
 }
 
 void CCubePushScript::SaveToFile(ofstream& fout)
--- Project/Scripts/CCubePushScript.h
+++ Project/Scripts/CCubePushScript.h
@@ -1,87 +1,14 @@
 ﻿#pragma once
 #include <Engine/CScript.h>
 
-// OBB (Oriented Bounding Box) 구조체
-struct OBB
-{
-	Vector3				   center;
-	Vector3				   halfSizes;
-	std::array<Vector3, 3> axes; // Local axes
-
-	Vector3 getVertex(int index) const
-	{
-		Vector3 vertex = center;
-		if (index & 1)
-			vertex = vertex + axes[0] * halfSizes.x;
-		else
-			vertex = vertex - axes[0] * halfSizes.x;
-		if (index & 2)
-			vertex = vertex + axes[1] * halfSizes.y;
-		else
-			vertex = vertex - axes[1] * halfSizes.y;
-		if (index & 4)
-			vertex = vertex + axes[2] * halfSizes.z;
-		else
-			vertex = vertex - axes[2] * halfSizes.z;
-		return vertex;
-	}
-};
-
 class CCubePushScript : public CScript
 {
-private:
-	// 축을 기준으로 투영된 크기를 구하는 함수
-	float getProjectionRadius(const OBB& box, const Vector3& axis)
-	{
-		return box.halfSizes.x * std::abs(axis.Dot(box.axes[0])) + box.halfSizes.y * std::abs(axis.Dot(box.axes[1])) +
-			   box.halfSizes.z * std::abs(axis.Dot(box.axes[2]));
-	}
-
-	// 파고든 깊이를 계산하는 함수
-	Vector3 getPenetrationDepth(const OBB& player, const OBB& wall)
-	{
-		// 충돌 감지와 유사한 방식으로 할 수 있지만, 여기서는 간단하게 처리합니다.
-		Vector3 penetration = {0, 0, 0};
-
-		// 각 축에 대해 깊이를 계산합니다.
-		for (const auto& axis : player.axes)
-		{
-			float playerProj = getProjectionRadius(player, axis);
-			float wallProj	 = getProjectionRadius(wall, axis);
-			float distance	 = std::abs((wall.center - player.center).Dot(axis));
-
-			// 파고든 깊이를 계산합니다.
-			float overlap = (playerProj + wallProj) - distance;
-			if (overlap > 0)
-			{
-				penetration = penetration + axis * overlap;
-			}
-		}
-
-		return penetration;
-	}
-
-	// 플레이어를 밀어주는 함수
-	void resolveCollision(OBB& player, const OBB& wall, const Vector3& dir, float threshold)
-	{
-		Vector3 penetration = getPenetrationDepth(player, wall);
-
-		// 파고든 깊이가 threshold 이상인 경우
-		if (std::abs(penetration.x) > threshold || std::abs(penetration.y) > threshold ||
-			std::abs(penetration.z) > threshold)
-		{
-			// dir 방향의 반대로 밀어줌
-			Vector3 penetration_dir = {-dir.x, -dir.y, -dir.z};
-			player.center =
-				player.center +
-				(penetration_dir * max(max(std::abs(penetration.x), std::abs(penetration.y)), std::abs(penetration.z)));
-		}
-	}
+	void resolveCollision(CGameObject* _Player);
 
 public:
 	virtual void begin() override;
 	virtual void tick() override;
-	virtual void Overlap(CCollider2D* _Collider, CGameObject* _OtherObj, CCollider2D* _OtherCollider) override;
+	virtual void Overlap(CPhysX* _Collider, CGameObject* _OtherObj, CPhysX* _OtherCollider);
 	virtual void SaveToFile(ofstream& fout);
 	virtual void LoadFromFile(ifstream& fin);
 
